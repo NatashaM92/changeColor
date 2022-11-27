f1db79ca87f042906e81bd8cb596932f5a5c967c
Natalia <nata22808@mail.ru>
2022-11-27 17:34:44 +0300

    set up

--- HW 4/ViewController.swift
+++ HW 4/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var redLightLabel: UILabel!
+    @IBOutlet var greenLightLabel: UILabel!
+    @IBOutlet var blueLightLabel: UILabel!
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLightSlider: UISlider!
+    @IBOutlet var greenLightSlider: UISlider!
+    @IBOutlet var blueLightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightLabel.text = String(format: "%.2f", redLightSlider.value)
+        greenLightLabel.text = String(format: "%.2f", greenLightSlider.value)
+        blueLightLabel.text = String(format: "%.2f",blueLightSlider.value)
+    }
+    
+    func colorChange() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redLightSlider.value),
+            green: CGFloat(greenLightSlider.value),
+            blue: CGFloat(blueLightSlider.value),
+            alpha: 1
+        )
     }
 
-
+    @IBAction func redLightSliderAction() {
+        redLightLabel.text = String(format: "%.2f", redLightSlider.value)
+        colorChange()
+    }
+    
+    @IBAction func greenLightSliderAction() {
+        greenLightLabel.text = String(format: "%.2f", greenLightSlider.value)
+    }
+    
+    @IBAction func blueLightSliderAction() {
+        blueLightLabel.text = String(format: "%.2f",blueLightSlider.value)
+    }
 }
 
